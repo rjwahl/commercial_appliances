09360d1469c676c5243562fe84a31dbb9927d902
Bob Wahl <rjwahl@hotwater.com>
2019-07-31 15:23:01 -0400

    Initial population of repository.

---
+++ make_standard_symlinks.bat
@@ -0,0 +1,48 @@
+mklink /J 00cvr_pge E:\Documents\Ontology\00cvr_pge
+
+mklink /J 05prolog E:\Documents\Ontology\05prolog 
+
+mklink /J 10gnrl_sfty_infrmtn E:\Documents\Ontology\10gnrl_sfty_infrmtn
+
+
+mklink /J 20intrdctn E:\Documents\Ontology\20intrdctn
+
+mklink /J 30ftrs_and_cmpnnts E:\Documents\Ontology\30ftrs_and_cmpnnts
+
+mklink /J 40instlltn_cnsdrtns E:\Documents\Ontology\40instlltn_cnsdrtns
+
+mklink /J 50instlltn_rqrmnts E:\Documents\Ontology\50instlltn_rqrmnts
+
+mklink /J 60instlltn_rqrmnts_cmmnwlth_of_msschstts E:\Documents\Ontology\60instlltn_rqrmnts_cmmnwlth_of_msschstts
+
+mklink /J 70vntng_instlltn E:\Documents\Ontology\70vntng_instlltn
+
+mklink /J 80wtr_htr_instlltn E:\Documents\Ontology\80wtr_htr_instlltn
+
+mklink /J 90tmprtre_rgltn E:\Documents\Ontology\90tmprtre_rgltn
+
+mklink /J 100cntrl_systm_oprtn E:\Documents\Ontology\100cntrl_systm_oprtn
+
+mklink /J 110strt_up E:\Documents\Ontology\110strt_up
+
+mklink /J 115trble_shtng E:\Documents\Ontology\115trble_shtng
+
+mklink /J 120mntnnce E:\Documents\Ontology\120mntnnce
+
+mklink /J 130dgrms E:\Documents\Ontology\130dgrms
+
+mklink /J 140bckmttr E:\Documents\Ontology\140bckmttr
+
+mklink /J brand_logos E:\Documents\Ontology\brand_logos
+
+mklink /J certification_logos E:\Documents\Ontology\certification_logos
+
+mklink /J general_grphcs E:\Documents\Ontology\gnrl_grphcs
+
+mklink /J hzrd_sttmnts E:\Documents\Ontology\hzrd_sttmnts
+
+mklink /J prdct_wrrnty E:\Documents\Ontology\prdct_wrrnty
+
+mklink /J proc_steps E:\Documents\Ontology\proc_steps
+
+mklink /J procs E:\Documents\Ontology\procs
---
+++ symlinks/make_standard_symlinks.bat
@@ -0,0 +1,48 @@
+mklink /J 00cvr_pge C:\Users\rjwahl\Documents\Ontology\00cvr_pge
+REM Copy this bat file to the destination folder and run to create symbolic links 
+REM to the source file repository.
+
+mklink /J 05prolog C:\Users\rjwahl\Documents\Ontology\05prolog 
+
+mklink /J 10gnrl_sfty_infrmtn C:\Users\rjwahl\Documents\Ontology\10gnrl_sfty_infrmtn
+
+
+mklink /J 20intrdctn C:\Users\rjwahl\Documents\Ontology\20intrdctn
+
+mklink /J 30ftrs_and_cmpnnts C:\Users\rjwahl\Documents\Ontology\30ftrs_and_cmpnnts
+
+mklink /J 40instlltn_cnsdrtns C:\Users\rjwahl\Documents\Ontology\40instlltn_cnsdrtns
+
+mklink /J 50instlltn_rqrmnts C:\Users\rjwahl\Documents\Ontology\50instlltn_rqrmnts
+
+mklink /J 60instlltn_rqrmnts_cmmnwlth_of_msschstts C:\Users\rjwahl\Documents\Ontology\60instlltn_rqrmnts_cmmnwlth_of_msschstts
+
+mklink /J 70vntng_instlltn C:\Users\rjwahl\Documents\Ontology\70vntng_instlltn
+
+mklink /J 80wtr_htr_instlltn C:\Users\rjwahl\Documents\Ontology\80wtr_htr_instlltn
+
+mklink /J 90tmprtre_rgltn C:\Users\rjwahl\Documents\Ontology\90tmprtre_rgltn
+
+mklink /J 100cntrl_systm_oprtn C:\Users\rjwahl\Documents\Ontology\100cntrl_systm_oprtn
+
+mklink /J 110strt_up C:\Users\rjwahl\Documents\Ontology\110strt_up
+
+mklink /J 115trble_shtng C:\Users\rjwahl\Documents\Ontology\115trble_shtng
+
+mklink /J 120mntnnce C:\Users\rjwahl\Documents\Ontology\120mntnnce
+
+mklink /J 130dgrms C:\Users\rjwahl\Documents\Ontology\130dgrms
+
+mklink /J 140bckmttr C:\Users\rjwahl\Documents\Ontology\140bckmttr
+
+mklink /J general_grphcs C:\Users\rjwahl\Documents\Ontology\grphcs
+
+mklink /J hzrd_sttmnts C:\Users\rjwahl\Documents\Ontology\hzrd_sttmnts
+
+mklink /J maps C:\Users\rjwahl\Documents\Ontology\maps
+
+mklink /J prdct_wrrnty C:\Users\rjwahl\Documents\Ontology\prdct_wrrnty
+
+mklink /J proc_steps C:\Users\rjwahl\Documents\Ontology\proc_steps
+
+mklink /J procs C:\Users\rjwahl\Documents\Ontology\procs
